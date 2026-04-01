52d2e76b4a577d622d694357ac3578e60e669a06
ig0moon <riders.igo407@gmail.com>
2026-04-01 20:36:57 -0300

    link pages + about + banners

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php
+include 'inc/header.inc.php'; 
+?>
+
+<div class="sobre">
+
+    <h3>MISSÃO</h3>
+        <p>Desenvolver instrumentos musicais inteligentes que integrem tecnologia e inteligência artificial para ampliar a criatividade, 
+            acelerar o aprendizado e transformar a forma como as pessoas se relacionam com a música.
+        </p>
+    <br>
+
+    <h3>VISÃO</h3>
+        <p>Tornar-se referência global em tecnologia aplicada à música, criando um ecossistema onde instrumentos, software e inteligência 
+        artificial trabalham juntos para redefinir a experiência musical.
+    </p>
+    <br>
+
+<h3>VALORES</h3>
+<div class="valores">
+    <ul>
+        <li>Inovação contínua</li>
+        <li>Foco no usuário</li>
+        <li>Acessibilidade musical</li>
+        <li>Qualidade e precisão</li>
+        <li>Integração tecnológica</li>
+        <li>Criatividade como prioridade</li>
+        <li>Escalabilidade e visão de negócio</li>
+    </ul>
+</div>
+
+</div>
--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 include 'inc/header.inc.php'; 
 ?>
 
-<?php
-include 'inc/body.inc.php';
-?>
+
+<div class="banner">
+    <img src="./img/banner_guitair.png" alt="Banner GuitAIr">
+    <img src="./img/banner_guitair2.png" alt="Banner GuitAIr">
+</div>
+
 
 <?php
 include 'inc/footer.inc.php';
