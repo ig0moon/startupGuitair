d09830efacc495f61fcd19609000c04877c69ad9
ig0moon <riders.igo407@gmail.com>
2026-04-02 20:29:52 -0300

    about bubbles + gradient

--- about.php
+++ about.php
@@ -3,30 +3,42 @@ include 'inc/header.inc.php';
 ?>
 
 <div class="sobre">
+    <div class="abt-bbl">
+        <br>
+        <h3>MISSÃO</h3>
+        <br>
 
-    <h3>MISSÃO</h3>
         <p>Desenvolver instrumentos musicais inteligentes que integrem tecnologia e inteligência artificial para ampliar a criatividade, 
             acelerar o aprendizado e transformar a forma como as pessoas se relacionam com a música.
         </p>
-    <br>
+    </div>
+    
+    <div class="abt-bbl">
+        <br>
+        <h3>VISÃO</h3>
+        <br>
 
-    <h3>VISÃO</h3>
         <p>Tornar-se referência global em tecnologia aplicada à música, criando um ecossistema onde instrumentos, software e inteligência 
-        artificial trabalham juntos para redefinir a experiência musical.
-    </p>
-    <br>
-
-<h3>VALORES</h3>
-<div class="valores">
-    <ul>
-        <li>Inovação contínua</li>
-        <li>Foco no usuário</li>
-        <li>Acessibilidade musical</li>
-        <li>Qualidade e precisão</li>
-        <li>Integração tecnológica</li>
-        <li>Criatividade como prioridade</li>
-        <li>Escalabilidade e visão de negócio</li>
-    </ul>
-</div>
-
+            artificial trabalham juntos para redefinir a experiência musical.
+        </p>
+    </div>
+    
+    <div class="abt-bbl">
+        <br>
+        <h3>VALORES</h3>
+        <br>
+        
+        <div class="valores">
+            <ul>
+                <li>- Inovação contínua</li>
+                <li>- Foco no usuário</li>
+                <li>- Acessibilidade musical</li>
+                <li>- Qualidade e precisão</li>
+                <li>- Integração tecnológica</li>
+                <li>- Criatividade como prioridade</li>
+                <li>- Escalabilidade e visão de negócio</li>
+            </ul>
+        </div>
+    </div>
+    
 </div>
