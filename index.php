65422f30bf6f73515422beee8fc50dd11015e999
ig0moon <riders.igo407@gmail.com>
2026-04-23 20:29:50 -0300

    about + index + form

--- index.php
+++ index.php
@@ -3,12 +3,37 @@ include 'inc/header.inc.php';
 ?>
 
 <div class="container">
+
     <div class="banner">
         <img src="./img/banner_guitair.png" alt="Banner GuitAIr">
         <img src="./img/banner_guitair2.png" alt="Banner GuitAIr">
     </div>
 
+    <br>
+
     <h3 class="titleAI">BEM-VINDO AO GUIT<span class="gradientAI">AI</span>R</h3>
+
+
+    <form class="instrumentos" method="POST" action="sub.php">
+        <fieldset class="modelo">
+            <legend>Conheça o GuitAIr</legend>
+            <input class="chkimg" type="radio" name="modelo" value="preto" id="preto" checked>
+            <label for="preto"><img src="img/violao.png" alt="Violão"></label>
+
+            <input class="chkimg" type="radio" name="modelo" value="preto" id="azul" checked>
+            <label for="azul"><img src="img/guitarra.png" alt="Guitarra"></label>
+
+            <input class="chkimg" type="radio" name="modelo" value="preto" id="vermelho" checked>
+            <label for="vermelho"><img src="img/bateria.png" alt="Bateria"></label>
+        </fieldset>
+    </form>
+
+    <br>
+    <br>
+
+    <div class="container-content">
+
+    </div>
 
 </div>
 
