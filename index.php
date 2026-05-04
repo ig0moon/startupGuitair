951003f3689d721b97bab1a2decc6d76101718e3
ig0moon <riders.igo407@gmail.com>
2026-05-04 20:34:50 -0300

    bug fixes + slideshow instrumentos

--- index.php
+++ index.php
@@ -14,20 +14,22 @@ include 'inc/header.inc.php';
 
     <h3 class="titleAI">BEM-VINDO AO GUIT<span class="gradientAI">AI</span>R</h3>
 
+    <br>
 
-    <form class="instrumentos" method="POST" action="sub.php">
-        <fieldset class="modelo">
-            <legend>Conheça o GuitAIr</legend>
-            <input class="chkimg" type="radio" name="modelo" value="preto" id="preto" checked>
-            <label for="preto"><img src="img/violao.png" alt="Violão"></label>
+    <div class="div-inst">
+        <form class="instrumentos" method="POST" action="sub.php">
+            <fieldset class="modelo">
+                <input class="chkimg" type="radio" name="modelo" value="amarelo" id="amarelo" checked>
+                <label for="amarelo"><img src="img/violao.png" alt="Violão"></label>
 
-            <input class="chkimg" type="radio" name="modelo" value="preto" id="azul" checked>
-            <label for="azul"><img src="img/guitarra.png" alt="Guitarra"></label>
+                <input class="chkimg" type="radio" name="modelo" value="azul" id="azul" checked>
+                <label for="azul"><img src="img/guitarra.png" alt="Guitarra"></label>
 
-            <input class="chkimg" type="radio" name="modelo" value="preto" id="vermelho" checked>
-            <label for="vermelho"><img src="img/bateria.png" alt="Bateria"></label>
-        </fieldset>
-    </form>
+                <input class="chkimg" type="radio" name="modelo" value="vermelho" id="vermelho" checked>
+                <label for="vermelho"><img src="img/bateria.png" alt="Bateria"></label>
+            </fieldset>
+        </form>
+    </div>
 
     <br>
     <br>
